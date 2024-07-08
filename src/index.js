ae79fa6943f03726e57e34e4f88dd77cc87dde48
KomfortKimko <komfortkimk@gmail.com>
2024-07-08 09:37:41 +0300

    Tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const taskDescription = document.getElementById('new-task-description').value;
+    if (taskDescription) {
+      const newTask = document.createElement('li');
+      newTask.innerHTML = taskDescription;
+      taskList.appendChild(newTask);
+
+      // Clear the input field after adding the task
+      document.getElementById('new-task-description').value = '';
+    }
+  });
 });
